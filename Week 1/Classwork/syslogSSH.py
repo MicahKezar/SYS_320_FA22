92e1ddb1db91accf7885397d19285c68523a03d6
micahkezar <micah.kezar@mymail.champlain.edu>
2022-09-05 15:07:50 -0400

    Week 1 Classwork Part II

---
+++ Week 1/Classwork/syslogSSH.py
@@ -0,0 +1,32 @@
+import syslogCheck
+import importlib
+importlib.reload(syslogCheck)
+
+# SSH authentication failures
+def ssh_fail(filename, searchTerms):
+
+    # Call syslogCheck and return results
+    is_found = syslogCheck._syslog(filename,searchTerms)
+
+    # found list
+    found = []
+
+    # Loop through results
+    for eachFound in is_found:
+
+        #print(eachFound)
+        # Split the results
+        sp_results = eachFound.split(" ")
+
+        # Append the split values to the found list
+        found.append(sp_results[8])
+    
+    # Remove duplicates by using set
+    # and convert list to dictionary
+
+    hosts = set(found)
+
+    # Print results
+    for eachHost in hosts:
+
+        print(eachHost)
---
+++ Week 1/Classwork/syslogSU.py
@@ -0,0 +1,33 @@
+import syslogCheck
+import syslogSU
+import importlib
+importlib.reload(syslogCheck)
+
+# SSH authentication failures
+def su_open(filename, searchTerms):
+
+    # Call syslogCheck and return results
+    is_found = syslogCheck._syslog(filename,searchTerms)
+
+    # found list
+    found = []
+
+    # Loop through results
+    for eachFound in is_found:
+
+        #print(eachFound)
+        # Split the results
+        sp_results = eachFound.split(" ")
+
+        # Append the split values to the found list
+        found.append(sp_results[5])
+    
+    # Remove duplicates by using set
+    # and convert list to dictionary
+
+    returnedValues = set(found)
+
+    # Print results
+    for eachValue in returnedValues:
+
+        print(eachValue)
